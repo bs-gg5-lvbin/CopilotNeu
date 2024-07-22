72f5b0c8fb3676248672e862aa74faa47fea208b
bin.lv@neusoft.com <lv200007274010!>
2024-07-22 19:25:38 +0800

    初版作成

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,42 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Application {
     public static void main(String[] args) {
-        System.out.println("001");
+        // 对Map进行求和
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(1, 1);
+        map.put(2, 2);
+        map.put(3, 3);
+        int sum = 0;
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            sum += entry.getValue();
+        }
+        System.out.println(sum);
+
+
+        int[] arr = {3, 2, 1};
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
+                }
+            }
+        }
+        for (int i : arr) {
+            System.out.println(i);
+        }
+
+
+
+
+
+
+
+
+
+
     }
 }
